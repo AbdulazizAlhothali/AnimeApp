c24671184ae9b8b0f1374b1d480b45e9a0b13ee2
HP <Abdulaziz.Alhothali.jed.k1@tuwaiq.edu.sa>
2021-12-29 07:22:15 +0300

    bottom navigation bar

--- app/src/main/java/com/example/animeapp/MainActivity.kt
+++ app/src/main/java/com/example/animeapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.animeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,21 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
             val navController = navHostFragment.navController
+            val navView: BottomNavigationView = findViewById(R.id.buttonView)
+            navView.setupWithNavController(navController)
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.signUpFragment -> {
+                        navView.visibility = View.GONE
+                    }
+                    R.id.signInFragment -> {
+                        navView.visibility = View.GONE
+                    }
+                    else -> {
+                        navView.visibility = View.VISIBLE
+                    }
+                }
+            }
         }
     }
 }
